a10a9a1fbb151afad4c46b76c650335098544413
Anthony Carton <mr.carton@gmail.com>
2018-09-08 16:42:24 -0600

    Spec 1 cat clicker

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+
+window.addEventListener('load', function() {
+	const BUTTON = document.querySelector('#cat-click');
+	BUTTON.addEventListener('click', function(){
+		let countValue = BUTTON.innerText;
+		countValue++;
+		BUTTON.innerText = countValue;
+	});	
+});
